f609c4c6adc1ae801dd6760c46a373f604a31894
Nick Hart <nhartmusic@gmail.com>
2020-11-08 18:10:02 +0100

    lifecycle examples

--- 666/src/App.vue
+++ 666/src/App.vue
@@ -2,41 +2,25 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
+import Blogs from "./Blogs";
 export default {
   name: "app",
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends,
+    Blogs,
   },
   data() {
     return {
       title: "Smell My Hair Daddy Joe ;)",
-      friends: [
-        { name: "Hunter", online: true },
-        { name: "Elizabeth", online: false },
-        { name: "Kamala", online: true },
-        { name: "Donald", online: false },
-      ],
     };
   },
-  methods: {
-    deleteFriend(payload) {
-      console.log(payload);
-      this.friends = this.friends.filter((friend) => {
-        return friend.name !== payload.name;
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
