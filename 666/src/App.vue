17a6ce28e0d1f65ab2113995db71ac646d440361
Nick Hart <nhartmusic@gmail.com>
2020-11-08 17:53:23 +0100

    firing custom events, learning about lifecycles now

--- 666/src/App.vue
+++ 666/src/App.vue
@@ -2,20 +2,40 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
 export default {
   name: "app",
   components: {
     Navbar,
+    OnlineFriends,
+    AllFriends,
   },
   data() {
     return {
       title: "Smell My Hair Daddy Joe ;)",
+      friends: [
+        { name: "Hunter", online: true },
+        { name: "Elizabeth", online: false },
+        { name: "Kamala", online: true },
+        { name: "Donald", online: false },
+      ],
     };
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload);
+      this.friends = this.friends.filter((friend) => {
+        return friend.name !== payload.name;
+      });
+    },
   },
 };
 </script>
