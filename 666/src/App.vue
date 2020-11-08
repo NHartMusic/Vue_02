e39d975b2018f584e1661366277f17cd699aa186
Nick Hart <nhartmusic@gmail.com>
2020-11-08 17:05:01 +0100

    whoops

---
+++ 666/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      title: "Smell My Hair Daddy Joe ;)",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
